6e29a3fb32590304afd31e1c6434e07a4467107f
edel <eg377@rutgers.edu>
2019-12-20 12:43:40 -0500

    repo

---
+++ Sunshine-Equipment/src/main/java/repository/EquipmentRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import model.Equipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
+}
