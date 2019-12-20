aa3bdec8c2a14ea3fde3f2c8dbe65d3ea66dbffe
William King <wpk12345@gmail.com>
2019-12-20 14:28:26 -0500

    office crud

--- Sunshine-Office-Crud/src/main/resources/data.sql
+++ Sunshine-Office-Crud/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO Office (office_name, street_address, city, zip) values
-('Delivery Center', '5001 Statesman Drive Suite 175 & 200', 'Irving', '75063' );
-INSERT INTO Office (office_name, street_address, city, zip) values
+INSERT INTO Office (office_name, street_address, city, state, zip, country) values
+('Delivery Center', '5001 Statesman Drive Suite 175 & 200', 'Irving', 'TX', '75063', 'United States');
+INSERT INTO Office (office_name, street_address, city, state, zip, country) values
 ('Cognizant Technology Solutions World Headquarters',
-'Glenpointe Centre West 500 Frank W Burr Blvd', 'Teaneck', '07666' );
-INSERT INTO Office (office_name, street_address, city, zip) values
-('Office', '200 Somerset Corporate BlvdSuite 8001', 'Bridgewater (New York metro)', '08807' );
+'Glenpointe Centre West 500 Frank W Burr Blvd', 'Teaneck', 'NJ', '07666', 'United States');
+INSERT INTO Office (office_name, street_address, city, state, zip, country) values
+('Office', '200 Somerset Corporate BlvdSuite 8001', 'Bridgewater (New York metro)', 'NJ', '08807', 'United States');
